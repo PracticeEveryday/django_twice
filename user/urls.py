c0704b153bb072ab92e907ea9297edbba1c37753
PracticeEveryday <developerforus@gmail.com>
2024-03-14 21:17:29 +0900

    feat: 로그인 API

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import RegisterAPIView, UserLoginApiView
 
-from .views import RegisterView, RegisterAPIView
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
+    path('login/', UserLoginApiView.as_view()),
     path("", include(router.urls))
 ]
 
--- user/views.py
+++ user/views.py
@@ -4,6 +4,8 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user.serializers import RegisterSerializer
 
@@ -28,3 +30,6 @@ class RegisterAPIView(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
