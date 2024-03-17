f151992c637aa05a3475be15ee2f88a7a3a3ef13
PracticeEveryday <developerforus@gmail.com>
2024-03-17 16:50:18 +0900

    feat: refreshToken 생성

--- custom_user/urls.py
+++ custom_user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-
+from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework import routers
 from .views import *
 
@@ -7,6 +7,7 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path("auth/", AuthAPIView.as_view()),
+    path("auth/refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
     path("register/", RegisterAPIView.as_view()),
     path("", include(router.urls))
 ]
--- custom_user/views.py
+++ custom_user/views.py
@@ -9,7 +9,6 @@ from django.shortcuts import get_object_or_404
 
 from django.conf import settings
 
-
 from .serializers import *
 from .models import CustomUser
 
