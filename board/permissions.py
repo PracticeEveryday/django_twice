684bff876a74d593a445d033a45a233e5e15f338
PracticeEveryday <developerforus@gmail.com>
2024-03-17 17:22:32 +0900

    feat: 권한 설정 수정

--- board/permissions.py
+++ board/permissions.py
@@ -6,4 +6,4 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.id == request.user.id
+        return obj.user.id == request.user.id
