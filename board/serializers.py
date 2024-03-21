a5637ebc21b80050f3c10fe9de81c8584222089b
PracticeEveryday <developerforus@gmail.com>
2024-03-21 16:15:14 +0900

    [MAIN] board return 타입 지정

--- board/serializers.py
+++ board/serializers.py
@@ -7,3 +7,12 @@ class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = ['title', 'content']
+
+
+    def to_representation(self, instance):
+        return {
+            "id": instance.id,
+            "title": instance.title,
+            "content": instance.content,
+            "create_ts": instance.create_ts
+        }
