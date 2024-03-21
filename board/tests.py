542723f08db3a9bb4a4b8b7196130cd505d22671
PracticeEveryday <developerforus@gmail.com>
2024-03-21 16:59:56 +0900

    feat: api_view testcode를 위해 setup에 최초 보드 생성

--- board/tests.py
+++ board/tests.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.tokens import AccessToken
 
 from custom_user.models import CustomUser
 from .models import Board
+from .permissions import UpdateOwnProfile
 
 
 class BoardApiTest(TestCase):
@@ -14,20 +15,27 @@ class BoardApiTest(TestCase):
         self.client = APIClient()
         # User 생성 및 인증 사용자 지정
         self.user = CustomUser.objects.create_user(email="test@test.com", password="test12345")
-        # self.client.force_authenticate(user=self.user)
+
+        # header에 토큰 추가
         self.access_token = AccessToken.for_user(self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.access_token))
+        Board.objects.create(title="Sample Title", content="Sample Content", user=self.user)
 
-
-def test_create_board(self):
-        url = "/board/api_view/"
+    def test_viewsets_create_board(self):
+        url = "/board/viewsets/"
         data = {'title': "BOARD TEST TITLE", 'content': "BOARD CONTENT"}
+
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Board.objects.count(), 1)
-        self.assertEqual(Board.objects.get().title, 'BOARD TEST TITLE')
-        self.assertEqual(Board.objects.get().content, 'BOARD CONTENT')
-        self.assertEqual(Board.objects.get().id, 1)
+        self.assertEqual(Board.objects.count(), 2)
+        self.assertEqual(Board.objects.last().title, 'BOARD TEST TITLE')
+        self.assertEqual(Board.objects.last().content, 'BOARD CONTENT')
+        self.assertEqual(Board.objects.last().id, 2)
 
-    # def test_get_board(self):
-    #     url = "/board/api_view/1"
+    def test_get_board(self):
+        url = "/board/api_view/"
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Board.objects.count(), 1)
+
